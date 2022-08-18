a74bb33422b1d3bae1c46bdc460b71db0a11642c
SAILgaosai <g.sail@qq.com>
2022-08-19 00:38:58 +0800

    tips update

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import valence_signal, { valence_signal_syn } from './charts/valence_signal'
 import arousal_signal, { arousal_signal_syn } from './charts/arousal_signal'
 import rppg_signal from './charts/rppg_signal'
 import rppg_power, { rppg_sync } from './charts/rppg_power'
-import gantt from './charts/gantt'
+import gantt, { dialog, types, types2 } from './charts/gantt'
 import { brain, heart, behavior } from './charts/score'
 import raddar from './charts/raddar'
 import ov from './charts/overview'
@@ -25,15 +25,18 @@ import { DateFormat } from "./utils";
   <div class="top">
     <img src="./assets/Logo-white.svg" class="logo" alt="logo" />
     <div class="toptext">
-      <div>Section Start Time: {{ DateFormat(new Date("2020-04-05 08:00:00"), 'yyyy E ddth HH:MM PP') }}</div>
-      <div>Duration: {{ 50 }}min</div>
+      <div><strong>Syneurgy Team Performance Report</strong></div>
+      <div>Section Start Time: {{ DateFormat(new Date("2020-04-05 08:00:00"), 'yyyy E ddth HH:MM PP') }} | Duration: {{
+          50
+      }}min</div>
     </div>
   </div>
   <div class="con">
+    <img src="./assets/team_members.jpg" alt="" style="margin-top:2em">
     <div class="overall-title">Overall Performance</div>
     <div class="overall-head">total score</div>
     <div class="overall-score">{{ 87 }}</div>
-    <Star class="stars" :score="4" />
+    <!-- <Star class="stars" :score="4" /> -->
 
     <div class="overall-des">Great job! Your team meeting scores 87 out of 100.</div>
     <div class="thr">
@@ -42,50 +45,90 @@ import { DateFormat } from "./utils";
       <Echarts class="echarts" :opt="behavior" :height="300" :width="300" />
     </div>
 
+    <div class="title1">High Level Insights</div>
+    <div class="two-col">
+      <div>
+        What the team did well:
+        <ul>
+          <li>Active Listening</li>
+          <li>Alignment and Engagement</li>
+          <li>Took ownership</li>
+        </ul>
+      </div>
+      <!--  -->
+      <div>
+        What the team can improve:
+        <ul>
+          <li>Clarity</li>
+          <li>Participation</li>
+          <li>Sentiment</li>
+        </ul>
+      </div>
+    </div>
     <Echarts class="echarts" :opt="ov" style="width:100%; aspect-ratio: 3/1;" />
     <div class="ovdes">
       <p>
         a. Lorem ipsum dolor sit amet, consectetuer adipiscing e, quis nostrud exerci </p>
-        <p>
+      <p>
         b. dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam
 
-        </p>
-        c. nostrud exerci tationum zzril delenit augue duis dolore
-        <p></p>
-     
+      </p>
+      c. nostrud exerci tationum zzril delenit augue duis dolore
+      <p></p>
+
 
     </div>
 
-    <div class="overall-title">Tips</div>
+    <div class="overall-title">Tips:</div>
     <ol class="tips">
-      <li>Lorem ipsum dolor sit amet, consectetuer adipiscing e, quis nostrud exerci tationum zzril delenit augue duis dolore</li>
-      <li>Consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tationum zzril delenit augue duis dolore</li>
-      <li>Nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tationum zzril delenit augue duis dolore</li>
-   </ol>
+      <li>Lorem ipsum dolor sit amet, consectetuer adipiscing e, quis nostrud exerci tationum zzril delenit augue duis
+        dolore</li>
+      <li>Consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+        volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tationum zzril delenit augue duis dolore</li>
+      <li>Nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+        quis nostrud exerci tationum zzril delenit augue duis dolore</li>
+    </ol>
+
+
 
     <h3 style="color:#6E974D">
-     <img class="ilogo" src="./assets/hand-with-bg.svg" />
-      2D valence-arousal
-    </h3>
-    <Echarts class="echarts" :opt="heatmap" style="width:40em; aspect-ratio: 1.2/1;" />
-
-    <div class="spline"></div>
-
-    <h3 style="color:#6E974D">
-     <img class="ilogo" src="./assets/hand-with-bg.svg" />
+      <img class="ilogo" src="./assets/hand-with-bg.svg" />
       RADDAR
     </h3>
     <Echarts class="echarts" :opt="raddar" style="width:40em; aspect-ratio: 1/1;" />
 
     <div class="spline"></div>
 
-<h3 style="color:#6B2524">
-      <img class="ilogo" src="./assets/brain-with-bg.svg" />
-    Brain Frenquency Variance Synchrony
+
+    <h3 style="color:#6E974D">
+      <img class="ilogo" src="./assets/hand-with-bg.svg" />
+      2D valence-arousal
     </h3>
+    <Echarts class="echarts" :opt="heatmap" style="width:40em; aspect-ratio: 1.2/1;" />
+
+
+    <div class="overall-title">Tips:</div>
+    <ul class="tips">
+      <li>Generally, the team listened to one another actively and well.</li>
+      <li>The team seemed to support one another and be open to ideas</li>
+    </ul>
+
+    <div class="spline"></div>
+
+
+    <h3 style="color:#DEA45E">
+      <img class="ilogo" src="./assets/brain-with-bg.svg" />
+      Brain Frenquency Variance Synchrony
+    </h3>
+    <br/>
+    <br/>
+
     <h3>
       Brain Frenquency Variance Signal
     </h3>
+    <br/>
+    <br/>
+
     <div class="spline"></div>
     <h3 style="color:#6B2524">
       <img class="ilogo" src="./assets/heart-with-bg.svg" />
@@ -100,27 +143,144 @@ import { DateFormat } from "./utils";
     <div class="spline"></div>
 
     <h3 style="color:#6E974D">
-     <img class="ilogo" src="./assets/hand-with-bg.svg" />
+      <img class="ilogo" src="./assets/hand-with-bg.svg" />
       Arousal Synchrony
     </h3>
     <Echarts class="echarts" :opt="arousal_signal_syn" style="width:100%; aspect-ratio: 3/1;" />
     <h3>Arousal signal</h3>
     <Echarts class="echarts" :opt="arousal_signal" style="width:100%; aspect-ratio: 3/1;" />
-
+    <div class="overall-title">Tips:</div>
+    <ul class="tips">
+      <li>The Team aligned on the need to make changes to PM and sales support</li>
+    </ul>
     <div class="spline"></div>
 
     <h3 style="color:#6E974D">
-    <img class="ilogo" src="./assets/hand-with-bg.svg" />
-    Valence Synchrony</h3>
+      <img class="ilogo" src="./assets/hand-with-bg.svg" />
+      Valence Synchrony
+    </h3>
     <Echarts class="echarts" :opt="valence_signal_syn" style="width:100%; aspect-ratio: 3/1;" />
     <h3>Valence signal</h3>
     <Echarts class="echarts" :opt="valence_signal" style="width:100%; aspect-ratio: 3/1;" />
 
     <div class="spline"></div>
 
-   
-    <h3>Speech overview</h3>
-    <Echarts class="echarts" :opt="gantt" style="width:100%; aspect-ratio: 2/1;" />
+
+    <h3>Sentiment Overview</h3>
+    <div class="legend">
+      <div class="legend-item" v-for="v, k in types" :key="k">
+        <div class="ff" :style="{ 'background-color': v }"></div>{{ k }}
+      </div>
+    </div>
+    <Echarts class="echarts" :opt="gantt" style="width:100%; aspect-ratio: 2.5/1;" />
+
+    <div class="spline"></div>
+
+    <h3>Language Use Overview</h3>
+    <div class="legend">
+      <div class="legend-item" v-for="v, k in types2" :key="k">
+        <div class="ff" :style="{ 'background-color': v }"></div>{{ k }}
+      </div>
+    </div>
+
+    <Echarts class="echarts" :opt="dialog" style="width:100%; aspect-ratio: 2.5/1;" />
+
+    <div class="overall-title">Tips:</div>
+    <ul class="tips">
+      <li>Leaders give opinions last, if at all</li>
+      <li>Team members should try to Interrupt less at this stage,
+        <ul>
+          <li>it seemed that some ppl on the team did not react well to burstiness, which indicates that the team was
+            still relatively trying to get to developing group norms</li>
+        </ul>
+      </li>
+      <li>Seek greater direct involvement of the team</li>
+      <li>Strive for more equal participation from every member of the group</li>
+      <li>More open ended questions vs statements of opinion</li>
+      <li>Team members should avoid talking over each other as much at this time</li>
+    </ul>
+
+    <div class="spline"></div>
+    <br />
+    <br />
+    <br />
+    <div class="title1">Supplementary Information:</div>
+    <div class="ultitle">What team did well:</div>
+    <ul class="tips">
+      <li>Team Lead took responsibility for some shortcomings and some missed objectives, setting the other members at
+        better ease</li>
+      <li>Seemed relatively new, but well formed team with a good communication style </li>
+      <li>Good acknowledgement from team for work put in by Sydney / P1</li>
+      <li>The team communicated thoughts and eventually aligned well</li>
+    </ul>
+    <div class="ultitle">What the team can improve:</div>
+    <ul class="tips">
+      <li>Allow the team to uncover and discover more before Team Lead and / or DRI gives their opinion</li>
+      <li>Seek greater clarity on what aligned with the organization’s objectives</li>
+      <li>Unpack the problem to solve more completely to avoid only servicing the symptoms, rather than the root causes
+      </li>
+      <li>It was relatively clear that something needed to be done to address the marketing issues vis a vis the rest of
+        the organization, specifically sales,
+        <ul>
+          <li>However, there was less clarity on what needed to be done, and to some degree, how to do it</li>
+        </ul>
+      </li>
+      <li>Team Leader and DRI must manage up the chain of command more effectively than they have been doing - as with
+        the VP</li>
+      <li>Stakeholders must establish the chain of command on this project / initiative</li>
+      <li>Strive for greater exploration of options on how to solve topics</li>
+      <li>Team / DRI needs to communicate with stakeholders with greater clarity</li>
+      <li>Ownership within the organization needs to be clearly established</li>
+      <li>Organization needs to improve communication and clarity up and down the chain of command</li>
+    </ul>
+    <div class="ultitle">Individual tips</div>
+    <br/>
+    <div class="ultitle">P1 / Sydney DRI - directly responsible individual - the buck stops here</div>
+    <ul class="tips">
+      <li>Headline then ask questions</li>
+      <li>Be less exasperated</li>
+      <li>Be more patient in explaining your work</li>
+      <li>Be more patient with your team members</li>
+      <li>Be more self aware -</li>
+      <li>Ask more open ended questions of the entire team</li>
+      <li>Take more time to uncover other obstacles to accomplishing the goal</li>
+      <li>Clearly define the goal</li>
+      <li>Clearly define why that’s important</li>
+      <li>Ask more questions</li>
+    </ul>
+
+    <div class="ultitle">P2 / PMM Lead - most senior person on the team</div>
+    <ul class="tips">
+      <li>Give opinions last</li>
+      <li>Survey team first</li>
+      <li>Be more self aware</li>
+      <li>Talk less</li>
+      <li>Ask more open ended questions</li>
+      <li>Help team clearly define goal</li>
+      <li>Help team clearly define why’s</li>
+    </ul>
+
+
+    <div class="ultitle">P3 /</div>
+    <ul class="tips">
+      <li>Listen more actively</li>
+      <li>Interrupt less at this stage of the teams development</li>
+      <li>Does not seem like P3 has earned the right to interrupt as yet</li>
+      <li>No mansplaining</li>
+      <li>Agree with non-verbal communication - nods, thumbs up</li>
+      <li>Keep note of your important points and give them at natural pause in the conversation with P1</li>
+    </ul>
+
+    <div class="ultitle">P4</div>
+    <ul class="tips">
+      <li>Face the camera</li>
+      <li>Looking sideways makes it seem that there is something more important than the meeting at hand</li>
+      <li>This person seems to be a bit guarded in his opinions</li>
+      <li>Move hand away from mouth when speaking</li>
+      <li>Seemed more decisive and powerful when facing the camera</li>
+      <li>Share opinions / thoughts more</li>
+      <li>Ask more questions to uncover information from the team</li>
+    </ul>
   </div>
 
 </template>
@@ -155,11 +315,12 @@ import { DateFormat } from "./utils";
   height: 100%;
 }
 
-.ilogo{
+.ilogo {
   height: 1.2em;
   position: relative;
   bottom: -0.2em;
 }
+
 .toptext {
   font-size: 115%;
   font-weight: normal;
@@ -180,9 +341,9 @@ import { DateFormat } from "./utils";
 }
 
 .overall-title {
-  font-size: 2em;
+  font-size: 1.6em;
   font-weight: bold;
-  font-size: 42px;
+  /* font-size: 42px; */
   color: #656565;
   text-align: left;
   margin: 1em 0;
@@ -211,11 +372,13 @@ import { DateFormat } from "./utils";
   color: #525353;
   margin: 1em auto;
 }
-h3{
+
+h3 {
   font-size: 125%;
   text-align: left;
   color: #A3A3A0;
 }
+
 .ovdes {
   /* font-size: 25px; */
   color: #525353;
@@ -223,27 +386,66 @@ h3{
   margin-left: 8em;
   text-align: left;
 }
-.ovdes p{
+
+.ovdes p {
   margin: 0;
 }
+
 .tips {
   text-align: left;
   font-weight: 300;
   color: #525353;
 }
-.tips li{
+
+.tips li {
   margin: 2em 0;
 }
+
+.ff {
+  width: 1em;
+  height: 1em;
+  display: inline-block;
+  margin: 0 0.5em;
+  position: relative;
+  bottom: -0.2em;
+}
+
+.legend {
+  width: 50em;
+  margin: auto;
+}
+
+.legend-item {
+  display: inline-block;
+}
+
+.two-col {
+  display: flex;
+  justify-content: space-evenly;
+  text-align: left;
+}
+
+.title1 {
+  font-size: 1.2em;
+  font-weight: bold;
+  margin: 0.5em;
+}
+
+.ultitle {
+  text-align: left;
+}
+
 @media print {
 
   .top {
-  width: 100%;
-   position:relative;
-  z-index: 999;
-}
-.con{
-  width:75%
-}
+    width: 100%;
+    position: relative;
+    z-index: 999;
+  }
+
+  .con {
+    width: 75%
+  }
 
 }
 </style>
--- src/charts/gantt.ts
+++ src/charts/gantt.ts
@@ -1,12 +1,13 @@
 // import spk from "../../data/gantt/spk.json";
 import spk from "../../data/gantt/new_speak_emotions.json";
+import dialogu from "../../data/gantt/new_speak_dialogueacts.json";
 import { graphic } from "echarts";
 let data = [];
 // var startTime = +new Date();
 
-let categories = spk.speakers;
+let categories = spk.speakers.sort().reverse();
 
-let types = {
+export let types = {
     'Neutral' : '#FF7410',
     'Positive' : '#2B9529',
     'Negative' : '#226BA9',
@@ -25,7 +26,7 @@ data = spk.data.map(e=>{
 })
 export default {
   tooltip: {
-    formatter: function (params) {
+    formatter: function (params:any) {
       return params.marker + params.name + ': ' + params.value[3]/1000 + ' s';
     }
   },
@@ -101,6 +102,113 @@ export default {
         y: 0
       },
       data: data
+    }
+  ]
+};
+
+let data2 = [];
+// var startTime = +new Date();
+
+let categories2 = dialogu.speakers.sort().reverse();
+
+export let types2 = {
+    'Uninterpretable' : '#84584E',
+    'Statement-opinion' : '#8E69B8',
+    'Statement-non-opinion' : '#C53932',
+    'Yes-No-Question' : '#D57DBE',
+    'Collaborative Completion' : '#EF8636',
+    'Abandoned or Turn-Exit' : '#3B75AF',
+    'Others' : '#529E3E',
+}
+
+data2 = dialogu.data.map(e=>{
+    return {
+        name: e.Resource,
+        value: [categories2.findIndex(i=>i==e.Task), e.Start, e.Finish, new Date(e.Finish) - new Date(e.Start),e.Resource],
+        itemStyle: {
+            color: types2[e.Resource]
+        }
+      }
+})
+
+export const dialog = {
+  tooltip: {
+    formatter: function (params:any) {
+      return params.marker + params.name + ': ' + params.value[3]/1000 + ' s';
+    }
+  },
+  dataZoom: [
+    {
+      type: 'slider',
+      filterMode: 'weakFilter',
+      showDataShadow: false,
+      top: 400,
+      labelFormatter: ''
+    },
+    {
+      type: 'inside',
+      filterMode: 'weakFilter'
+    }
+  ],
+  grid: {
+    height: 300
+  },
+  legend:{
+    data:Object.keys(types2)
+  },
+  xAxis: {
+    type:'time',
+    // min: startTime,
+    // max:startTime+3000*1000,
+    scale: true,
+    axisLabel: {
+      // formatter: function (val) {
+      //   return Math.max(0, val)/1000 + ' s';
+      // }
+    }
+  },
+  yAxis: {
+    data: categories2//.sort((a,b)=>a<b)
+  },
+  series: [
+    {
+      type: 'custom',
+      renderItem: function (params, api) {
+        var categoryIndex = api.value(0);
+        var start = api.coord([api.value(1), categoryIndex]);
+        var end = api.coord([api.value(2), categoryIndex]);
+        var height = api.size([0, 1])[1] * 0.6;
+        var rectShape = graphic.clipRectByRect(
+          {
+            x: start[0],
+            y: start[1] - height / 2,
+            width: end[0] - start[0],
+            height: height
+          },
+          {
+            x: params.coordSys.x,
+            y: params.coordSys.y,
+            width: params.coordSys.width,
+            height: params.coordSys.height
+          }
+        );
+        return (
+          rectShape && {
+            type: 'rect',
+            transition: ['shape'],
+            shape: rectShape,
+            style: api.style()
+          }
+        );
+      },
+      itemStyle: {
+        opacity: 0.8
+      },
+      encode: {
+        x: [1, 2],
+        y: 0
+      },
+      data: data2
     }
   ]
 };
